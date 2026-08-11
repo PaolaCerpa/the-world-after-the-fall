50435cb7bb208187fdbe9ac585dc08c254ceca40
PaolaCerpa <paola.cerpa16@gmail.com>
2026-08-10 19:06:14 -0500

    feat(analisis): agregar pagina de analisis

---
+++ src/app/analisis/page.tsx
@@ -0,0 +1,8 @@
+export default function AnalisisPage() {
+  return (
+    <div>
+      <h1>Análisis</h1>
+      <p>Aquí irán los articulos de análisis de El mundo depués del fin.</p>
+    </div>
+  );
+}
