fd1febc0e0c8655aa3ccac1b1ebac3715b5596dc
PaolaCerpa <paola.cerpa16@gmail.com>
2026-08-10 19:07:26 -0500

    feat(personajes): agregar pagina de personajes

---
+++ src/app/personajes/page.tsx
@@ -0,0 +1,8 @@
+export default function PersonajesPage() {
+    return (
+        <div>
+            <h1>Personajes</h1>
+            <p>Aquí irán la base de datos de personajes de El mundo depués del fin.</p>
+        </div>
+    );
+}
